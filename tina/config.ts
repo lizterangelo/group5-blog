23f3509d44487ded685af9e910010878e5392c38
lizterangelo <lmabrau@up.edu.ph>
2025-02-04 10:14:09 +0800

    new

--- tina/config.ts
+++ tina/config.ts
@@ -1,108 +1,55 @@
-import { CATEGORIES } from '../src/data/categories.ts'
-import { defineConfig } from 'tinacms'
+import { defineConfig } from "tinacms";
 
 // Your hosting provider likely exposes this as an environment variable
-// const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || 'main'
+// const branch =
+//   process.env.GITHUB_BRANCH ||
+//   process.env.VERCEL_GIT_COMMIT_REF ||
+//   process.env.HEAD ||
+//   "main";
 
 export default defineConfig({
-	branch: 'main',
-	clientId: 'bfaac734-45c0-469a-98be-df7da654b527', // Get this from tina.io
-	token: 'e22ed6422747f99b827b857a1a70e9970e1893ae', // Get this from tina.io
+  branch: 'main',
 
-	build: {
-		outputFolder: 'admin',
-		publicFolder: 'public'
-	},
-	media: {
-		tina: {
-			mediaRoot: '/src/assets/images',
-			publicFolder: ''
-		}
-	},
-	schema: {
-		collections: [
-			{
-				name: 'post',
-				label: 'Blog Post',
-				path: 'src/content/blog',
-				format: 'mdx',
-				fields: [
-					{
-						type: 'image',
-						label: 'Cover Image',
-						required: true,
-						name: 'heroImage',
-						description: 'The image used for the cover of the post'
-					},
+  // Get this from tina.io
+  // clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  clientId: "bfaac734-45c0-469a-98be-df7da654b527",
+  // Get this from tina.io
+  // token: process.env.TINA_TOKEN,
+  token:  "bfaac734-45c0-469a-98be-df7da654b527",
 
-					{
-						type: 'string',
-						required: true,
-						name: 'category',
-						label: 'Category',
-						description: 'Select an category for this post',
-						options: [...CATEGORIES]
-					},
-					{
-						type: 'string',
-						label: 'description',
-						required: true,
-						name: 'description',
-						description: 'A short description of the post'
-					},
-					{
-						type: 'datetime',
-						name: 'pubDate',
-						label: 'Publication Date',
-						required: true
-					},
-					{
-						name: 'draft',
-						label: 'Draft',
-						type: 'boolean',
-						description: 'If this is checked the post will not be published'
-					},
-					{
-						type: 'string',
-						name: 'tags',
-						required: true,
-						label: 'Tags',
-						description: 'Tags for this post',
-						list: true,
-						ui: {
-							component: 'tags'
-						}
-					},
-					{
-						type: 'string',
-						name: 'title',
-						label: 'Title',
-						isTitle: true,
-						required: true
-					},
-					{
-						type: 'rich-text',
-						label: 'Body',
-						name: 'SButton',
-						isBody: true,
-						templates: [
-							// Custom Components
-							{
-								label: 'SButton',
-								name: 'SButton',
-								fields: [
-									{
-										type: 'rich-text',
-										label: 'SButton',
-										name: 'children',
-										isBody: true
-									}
-								]
-							}
-						]
-					}
-				]
-			}
-		]
-	}
-})
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "public",
+    },
+  },
+  // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "content/posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
