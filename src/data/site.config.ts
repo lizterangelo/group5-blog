c4e4a88da282a7d171a15fa953534ebafee23822
lizterangelo <lmabrau@up.edu.ph>
2025-03-01 01:34:03 +0800

    commit

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -15,7 +15,7 @@ export const siteConfig: SiteConfig = {
 	title: 'The 5quills', // Site title.
 	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
 	lang: 'en-GB',
-	ogLocale: 'en_GB',
+	ogLocale: 'en_GB',		
 	shareMessage: 'Share this post', // Message to share a post on social media
 	paginationSize: 6 // Number of posts per page
 }
