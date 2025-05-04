cbdd02629d3b6c9a48c68e579bad8d45617f0df9
lizterangelo <lmabrau@up.edu.ph>
2025-05-05 00:51:38 +0800

    update

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -11,8 +11,8 @@ interface SiteConfig {
 
 export const siteConfig: SiteConfig = {
 	site: 'https://the5quills.vercel.app/', // Write here your website url
-	author: 'Group 5', // Site author
-	title: 'The 5quills', // Site title.
+	author: 'angela abrau', // Site author
+	title: 'angela abrau', // Site title.
 	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
 	lang: 'en-GB',
 	ogLocale: 'en_GB',		
